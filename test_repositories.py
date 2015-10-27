2668f9f7ef0928172695abe1e13ad851204ca8fc
Marc Hanheide <marc@hanheide.net>
2015-10-27 22:02:46 +0000

    added installation of further tools for testing

--- test_repositories.py
+++ test_repositories.py
@@ -69,7 +69,7 @@ def test_repositories(ros_distro, repo_list, version_list, workspace, test_depen
     else:
         # install stuff we need
         print("Installing Debian packages we need for running this script")
-        apt_get_install(['python-catkin-pkg', 'python-rosinstall', 'python-rosdistro', 'python-yaml'], sudo=sudo)
+        apt_get_install(['python-catkin-pkg', 'python-rosinstall', 'python-rosdistro', 'python-yaml', 'virtualgl', 'libav-tools', 'nvidia-common'], sudo=sudo)
 
     if ros_distro != 'fuerte':
         return _test_repositories(ros_distro, repo_list, version_list, workspace, test_depends_on,
