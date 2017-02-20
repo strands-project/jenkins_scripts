9109118dfd648fe5d4d7f96fe2e1aa6e524db277
Marc Hanheide <marc@hanheide.net>
2017-02-20 11:35:09 +0000

    Update test_repositories.py

--- test_repositories.py
+++ test_repositories.py
@@ -45,7 +45,7 @@ def test_repositories(ros_distro, repo_list, version_list, workspace, test_depen
         call("apt-get update")
         call("apt-get install ca-certificates --yes")
         call("apt-get install curl --yes")
-        kernel_vesion = os.uname()[2]
+        kernel_version = os.uname()[2]
         call("apt-get install --reinstall linux-image-" + kernel_version)
 
         # Add ros sources to apt
